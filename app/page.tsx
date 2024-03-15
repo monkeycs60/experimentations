0af9c4aeb0cc516a45f6ccd8309df65fd12d4d5e
clementserizay <clement.serizay@gmail.com>
2024-03-15 14:04:06 +0100

    Init project

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MainSection from '@/components/Home/MainSection';
+import Image from 'next/image';
+
+export default function Home() {
+	return (
+		<main>
+			<MainSection />
+		</main>
+	);
+}
