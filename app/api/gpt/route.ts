e20f63d7378fdd5319a7e51358ff25006564b973
monkeycs60 <clement.serizay@gmail.com>
2024-03-27 23:05:52 +0100

    fix: wrong env name

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import OpenAI from 'openai';
 
 const openai = new OpenAI({
-	apiKey: process.env.OPEN_API_KEY || '',
+	apiKey: process.env.OPEN_AI_KEY || '',
 });
 
 export const runtime = 'edge';
